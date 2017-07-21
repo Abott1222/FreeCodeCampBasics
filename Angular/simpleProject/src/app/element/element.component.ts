00d5f11151ad008a09ae65eef493b9824fa8d234
Alex Bott <Abott1222@gmail.com>
2017-07-21 12:23:29 -0700

    set up game basics and events

--- Angular/simpleProject/src/app/element/element.component.ts
+++ Angular/simpleProject/src/app/element/element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-element',
@@ -6,16 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./element.component.css']
 })
 export class ElementComponent implements OnInit {
+  //@Output() x: EventEmitter<any> = new EventEmitter(); ;
+
+  content:any;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+
+  
   markBox($event) {
-    var toBeChanged = event.target;
-    //type EventTarget
-    console.log(toBeChanged);
+    //type EventTarget but cast as HTMLElement
+    var toBeChanged = <HTMLScriptElement>event.target;
+
+    toBeChanged.innerHTML = "X";
+    //console.log(toBeChanged.classList);
+    //toBeChanged.style.textAlign = "center";
+    toBeChanged.style.padding = "0px 0 0 0";
   }
+  
 
 }
--- Angular/simpleProject/src/app/row/row.component.ts
+++ Angular/simpleProject/src/app/row/row.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./row.component.css']
 })
 export class RowComponent implements OnInit {
-
+  //@Input() x;
   constructor() { }
 
   ngOnInit() {
