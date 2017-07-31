c87631a53b7138c4b2c08b22fee317f4a724ee24
Alex Bott <Abott1222@gmail.com>
2017-07-31 15:40:19 -0700

    setting up todo for clone

--- Angular/momentumclone/src/app/app.module.ts
+++ Angular/momentumclone/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
-
+import { ToDoListComponent } from './toDo/to-do-list/to-do-list.component';
+import { ToDoElementComponent } from './toDo/to-do-element/to-do-element.component';
+import {ToDoService} from './toDo/to-do.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoListComponent,
+    ToDoElementComponent
   ],
   imports: [
     BrowserModule,
     MaterialModule
   ],
-  providers: [],
+  providers: [ToDoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
