3382d35318687d58619d7ac2e455e601dbd7bdd7
Alex Bott <Abott1222@gmail.com>
2017-08-12 12:37:02 -0700

    trying to set up http communication with api

--- Angular/momentumclone/src/app/weather.service.ts
+++ Angular/momentumclone/src/app/weather.service.ts
@@ -6,18 +6,31 @@ export class WeatherService {
   url = "api.openweathermap.org/data/2.5/weather?lat=";
   results: any;
   constructor(private http: HttpClient) { }
-
-  getWeather(geoLocation) {
-    this.url = this.url + geoLocation.coords.latitude;
-    this.url = this.url + "&lon=" + geoLocation.coords.longitude;
+  /*
+{"coord":{"lon":-122.95,"lat":47.05},
+"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01d"}],
+"base":"stations",
+"main":{"temp":292.36,"pressure":1015,"humidity":56,"temp_min":289.15,"temp_max":295.15},
+"visibility":16093,
+"wind":{"speed":2.1},
+"clouds":{"all":1},
+"dt":1502565300,
+"sys":{"type":1,"id":2925,"message":0.0057,"country":"US","sunrise":1502543194,"sunset":1502594744},
+"id":5805687,"name":"Olympia","cod":200}
+  */
+  getWeather(latit, longit) {
+    /*
+    this.url = this.url + latit;
+    this.url = this.url + "&lon=" + longit  ;
     this.url = this.url + "&APPID=695de6f7660449a1838322fa9f08abf3";
     this.http.get('/api/items').subscribe(data => {
       // Read the result field from the JSON response.
-      this.results = data['results'];
+      this.results = data['main'];
       alert("Got results!");
       console.log(this.results);
       return this.results;
     });
+    */
   }
 
 }
--- Angular/momentumclone/src/app/weather/weather.component.ts
+++ Angular/momentumclone/src/app/weather/weather.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {WeatherService} from "../weather.service";
+import {WeatherService} from '../weather.service';
 
 @Component({
   selector: 'app-weather',
@@ -8,22 +8,28 @@ import {WeatherService} from "../weather.service";
 })
 export class WeatherComponent implements OnInit {
   localWeather: any;
+  position:any;
 
   constructor(private weatherService: WeatherService) { }
 
   ngOnInit() {
+    let positionTemp;
     if ("geolocation" in navigator) {
       /* geolocation is available */
-      navigator.geolocation.getCurrentPosition(function(position) {
+      navigator.geolocation.getCurrentPosition(function(pos) {
         // position.coords.longitude/latitude
-        console.log(position);
-        this.localWeather = this.weatherService.getWeather(position);
+        console.log(pos);
+        positionTemp = pos;
         //console.log(this.localWeather);
       });
     } else {
       /* geolocation IS NOT available */
       /* return null to indicate error */
     }
+    this.position = positionTemp;
+    console.log(this.position);
+    this.localWeather = this.weatherService.getWeather(this.position.coords.latitude, this.position.coords.longitude);
+    console.log(this.localWeather);
 
   }
 
